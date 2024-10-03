6832573b218537a644372990f272fcc9e2fa3b1f
Mitja Podreka <mitja@kitaj.net>
2024-10-03 15:13:46 +0200

    Added Slovenian locale folder

---
+++ lang/sl/theme_boost_union_ff.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Boost Union FF - Language pack
+ *
+ * @package    theme_boost_union_ff
+ * @copyright  2024 Mitja Podreka <mitja.podreka@ff.uni-lj.si>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Let codechecker ignore some sniffs for this file as it is perfectly well ordered, just not alphabetically.
+// phpcs:disable moodle.Files.LangFilesOrdering.UnexpectedComment
+// phpcs:disable moodle.Files.LangFilesOrdering.IncorrectOrder
+
+// General.
+$string['pluginname'] = 'Boost Union FF';
+$string['choosereadme'] = 'Ta vtičnik je prilagoditev teme Boost Union za potrebe Filozofske fakultete v Ljubljani.';
+$string['configtitle'] = 'Boost Union FF';
+$string['settingsoverview_buc_desc'] = 'Z Boost Union FF temo Boost Union prilagajamo potrebam FF UL.';
+
+// Settings: General settings tab.
+// not translated as not needed
+
+/**************************************************************
+ * EXTENSION POINT:
+ * Add your language strings for your settings here.
+ *************************************************************/
+
+// Privacy API.
+$string['privacy:metadata'] = 'Tema Boost Union FF ne shranjuje nobenih podatkov o uporabnikih.';
+
